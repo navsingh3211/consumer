5cc0bb29208bff9a52297d6be3076f98e8c95967
navsingh3211 <navsingh3211@gmail.com>
2026-01-19 21:48:16 +0530

    feign client inter service com

--- src/main/java/com/demo/consumer/feign/FeignController.java
+++ src/main/java/com/demo/consumer/feign/FeignController.java
@@ -1,5 +1,13 @@
 package com.demo.consumer.feign;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feign")
 public class FeignController {
-    
+
+    public String getInstance(){
+        return "";
+    }
 }
