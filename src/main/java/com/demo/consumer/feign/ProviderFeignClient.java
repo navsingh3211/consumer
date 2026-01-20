5e7d46da114777e2e1518eca9f8c94113ff99bed
navsingh3211 <navsingh3211@gmail.com>
2026-01-20 18:13:05 +0530

    restClient interservice communication

--- src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
+++ src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
@@ -3,7 +3,7 @@ package com.demo.consumer.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="provider-service",url="http://localhost:8081")
+@FeignClient(name="provider",url="http://localhost:8081")
 public interface ProviderFeignClient {
 
     @GetMapping("/instance-info")
