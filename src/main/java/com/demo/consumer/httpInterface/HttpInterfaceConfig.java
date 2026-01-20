abf113c739098dc91d5e10fcffb8ac34e81926b3
navsingh3211 <navsingh3211@gmail.com>
2026-01-20 23:54:36 +0530

    http interface exchange interservice communication

---
+++ src/main/java/com/demo/consumer/httpInterface/HttpInterfaceConfig.java
@@ -0,0 +1,35 @@
+package com.demo.consumer.httpInterface;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class HttpInterfaceConfig {
+
+//    @Bean
+//    public ProviderHttpInterface webClientHttpInterface(){
+//        WebClient webClient = WebClient
+//                .builder().baseUrl("http://localhost:8081").build();
+//
+//        WebClientAdapter adapter = WebClientAdapter.create(webClient);
+//        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+//                .builderFor(adapter).build();
+//
+//        ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
+//        return service;
+//    }
+    @Bean
+    public ProviderHttpInterface webClientHttpInterface(){
+        RestClient restClient = RestClient.builder().baseUrl("http://localhost:8081").build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+        ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
+        return service;
+    }
+}
